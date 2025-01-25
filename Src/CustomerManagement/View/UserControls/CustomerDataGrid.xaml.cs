87359d0a07a2aad9b2086ddfe8fb4361fd88806e
scg984261 <scottgillen@hotmail.com>
2025-01-25 11:39:17 +0000

    Restoring the Customer Data Grid

---
+++ Src/CustomerManagement/View/UserControls/CustomerDataGrid.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using DataModel;
+
+namespace CustomerManagement.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CustomerDataGrid.xaml
+    /// </summary>
+    public partial class CustomerDataGrid : UserControl
+    {
+        public List<Customer> CustomerList { get; set; }
+        public ObservableCollection<Customer>? Customers { get; set; }
+
+        public CustomerDataGrid()
+        {
+            InitializeComponent();
+
+            this.CustomerList = new List<Customer>
+            {
+                new Customer
+                {
+                    Id = 1,
+                    CompanyName = "Albany House Business Centres LTD",
+                    BusinessContact = "Niall Gillen",
+                    EmailAddress = "niallgillen@hudsonhouse.co.uk",
+                    ContactNumber = "07919367388",
+                    IsActive = true,
+                    CreatedDateTime = DateTime.Now,
+                    LastUpdateDateTime = DateTime.Now
+                },
+                new Customer
+                {
+                    Id = 2,
+                    CompanyName = "Barclays Bank PLC",
+                    BusinessContact = "Scott Gillen",
+                    EmailAddress = "scott.gillen2@barclays.com",
+                    ContactNumber = "N/A",
+                    IsActive = true,
+                    CreatedDateTime = DateTime.Now,
+                    LastUpdateDateTime = DateTime.Now
+                }
+            };
+
+            this.Customers = new ObservableCollection<Customer>(CustomerList);
+        }
+
+        public void Window_Loaded(object sender, EventArgs e)
+        {
+            this.CustomerTable.ItemsSource = this.Customers;
+        }
+
+        private void CustomerTable_RowEditEnding(object sender, DataGridRowEditEndingEventArgs rowEventArgs)
+        {
+            Customer customerToUpdate = (Customer) this.CustomerTable.SelectedValue;
+            // this.DataProvider.UpdateCustomer(customerToUpdate);
+        }
+    }
+}
---
+++ Src/CustomerManagement/View/UserControls/MenuBar.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomerManagement.View.UserControls
+{
+    public partial class MenuBar : UserControl
+    {
+        public MenuBar()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItemOpen_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemExit_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to close?", "Close", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void NewCustomerButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+    }
+}
