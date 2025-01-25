035c709fa5f73f1268f2e2032dc6d37ac8f1f7db
scg984261 <scottgillen@hotmail.com>
2025-01-25 16:19:28 +0000

    Started on data loader

---
+++ Src/CustomerManagement/DataLoader/CsvDataLoader.cs
@@ -0,0 +1,13 @@
+﻿using CDB.Model;
+
+namespace CustomerManagement.DataLoader
+{
+    public class CsvDataLoader
+    {
+        public List<Customer> LoadCustomersFromFile(string filePath)
+        {
+            // Logic for converting rows in the file into customer objects.
+            return new List<Customer>();
+        }
+    }
+}
--- Src/CustomerManagement/View/UserControls/MenuBar.xaml.cs
+++ Src/CustomerManagement/View/UserControls/MenuBar.xaml.cs
@@ -1,18 +1,18 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
+using CustomerManagement.DataLoader;
 
 namespace CustomerManagement.View.UserControls
 {
     public partial class MenuBar : UserControl
     {
+        private CsvDataLoader dataLoader = new CsvDataLoader();
+
         public MenuBar()
         {
             InitializeComponent();
-        }
-
-        private void MenuItemOpen_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-
         }
 
         private void MenuItemExit_Click(object sender, System.Windows.RoutedEventArgs e)
@@ -22,6 +22,34 @@ namespace CustomerManagement.View.UserControls
             if (result == MessageBoxResult.Yes)
             {
                 Application.Current.Shutdown();
+            }
+        }
+
+        private void MenuItemOpen_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = @"C:\Users\Downloads";
+            bool? fileSelected = dialog.ShowDialog();
+
+            if (fileSelected == true)
+            {
+                string filePath = dialog.FileName;
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = filePath;
+                Process.Start(startInfo);
+            }
+        }
+
+        private void MenuItemLoadData_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = @"C:\Users\Downloads";
+            bool? fileSelected = dialog.ShowDialog();
+
+            if (fileSelected == true)
+            {
+                string filePath = dialog.FileName;
+                this.dataLoader.LoadCustomersFromFile(filePath);
             }
         }
 
